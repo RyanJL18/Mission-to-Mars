1a7d68846646d3d2d06e1f2c80548cc6f847cc0b
RyanJL18 <ryan.lynch1818@gmail.com>
2022-05-22 14:03:41 -0500

    adding delivereables

--- scraping.py
+++ scraping.py
@@ -14,12 +14,14 @@ def scrape_all():
     #browser = Browser('chrome', **executable_path, headless=False)
 
     news_title, news_paragraph = mars_news(browser)
+    hemisphere_image_url = hemispheres(browser)
     # Run all scraping functions and store results in dictionary
     data = {
       "news_title": news_title,
       "news_paragraph": news_paragraph,
       "featured_image": featured_image(browser),
       "facts": mars_facts(),
+      "hemispheres": hemisphere_image_urls,
       "last_modified": dt.datetime.now()
     }
 
